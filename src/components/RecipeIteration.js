9e698e3e79750ef60e594e4120921edd3f21f49e
rkrammes <rkrammes@mac.com>
2025-03-22 21:50:59 -0400

    Add RecipeIteration component to display current ingredients

---
+++ src/components/RecipeIteration.js
@@ -0,0 +1,17 @@
+@@ Create or update /Users/ryankrammes/kraft_ai/src/components/RecipeIteration.js
++// @flow
++import React from 'react';
++
++function RecipeIteration(): React$Node {
++  return (
++    <div className=\"card\">
++      <h2>Current Ingredients</h2>
++      <p>Distilled Water – 1 cup (240 mL)</p>
++      <p>Liquid Castile Soap – 2 tablespoons (30 mL)</p>
++      <p>Sweet Almond Oil – 1 teaspoon (5 mL)</p>
++      <p>Essential Oils – 10–15 drops</p>
++    </div>
++  );
++}
++
++export default RecipeIteration;
