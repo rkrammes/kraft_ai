79b056bfc5ba8d88f8676d24c59f684cdd935f76
rkrammes <rkrammes@mac.com>
2025-03-22 20:10:50 -0400

    Add App component and update ESLint cache

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+function App(): React$Node {
+  return (
+    <div className="App">
+      <h1>Welcome to Kraft AI</h1>
+      <p>This is the production-ready React app.</p>
+    </div>
+  );
+}
+
+export default App;
