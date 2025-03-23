ab854580b74d51e83f2c3d7c2735024322f3e4be
rkrammes <rkrammes@mac.com>
2025-03-23 15:08:46 -0400

    Refactor Root component to use TypeScript and improve type safety for props

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,27 @@ import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './theme';
 import App from './App';
 
+interface AppProps {
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppComponent = App as React.FC<AppProps>;
+
 const container = document.getElementById('root');
 if (!container) {
   throw new Error('Root container not found');
 }
 const root = ReactDOM.createRoot(container);
 
-function Root(): JSX.Element {
+function Root(): React.ReactElement {
   const [darkMode, setDarkMode] = useState(false);
   const theme = darkMode ? darkTheme : lightTheme;
 
   return (
     <React.StrictMode>
       <ThemeProvider theme={theme}>
-        <App darkMode={darkMode} setDarkMode={setDarkMode} />
+        <AppComponent darkMode={darkMode} setDarkMode={setDarkMode} />
       </ThemeProvider>
     </React.StrictMode>
   );
