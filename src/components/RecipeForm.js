cf7c9f22146b990f65d4849b65d2c5c7ca868437
rkrammes <rkrammes@mac.com>
2025-03-22 20:26:02 -0400

    Add RecipeForm component and update ESLint cache

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+const RecipeForm = (): React$Node => {
+  return (
+    <div>
+      <h2>Recipe Form</h2>
+      <p>Form to add or edit recipes will be here.</p>
+    </div>
+  );
+};
+
+export default RecipeForm;
