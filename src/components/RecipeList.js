a0ae5e2950a35f96d96fcb94eac4308efa3c900c
rkrammes <rkrammes@mac.com>
2025-03-22 20:23:14 -0400

    Add RecipeList component and update ESLint cache

---
+++ src/components/RecipeList.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+const RecipeList = (): React$Node => {
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      <p>List of recipes will be displayed here.</p>
+    </div>
+  );
+};
+
+export default RecipeList;
