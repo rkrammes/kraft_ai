c1ccf55c0b94850a4054a5c4608f68e2fc5635aa
rkrammes <rkrammes@mac.com>
2025-03-23 12:10:50 -0400

    Add TypeScript declaration files for styled-components and emotion packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function Root() {
+  // handle local darkMode state here, pass it to <App>
+  const [darkMode, setDarkMode] = useState(false);
+
+  // pick which theme tokens to use
+  const theme = darkMode ? darkTheme : lightTheme;
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App darkMode={darkMode} setDarkMode={setDarkMode} />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
+root.render(<Root />);
